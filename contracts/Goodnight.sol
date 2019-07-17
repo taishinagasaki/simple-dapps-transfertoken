8914eb2743c1dcef3fd45cd43fcf55badd42cc50
長崎太志 <tnagasaki@netprotections.co.jp>
2019-07-18 00:02:43 +0900

    [mod]goodnight solidity typo

--- contracts/Goodnight.sol
+++ contracts/Goodnight.sol
@@ -1,7 +1,7 @@
 pragma solidity >=0.4.21 <0.6.0;
 contract Goodnight {
   string public message;
-  contructor(string memory initMessage) public {
+  constructor(string memory initMessage) public {
     message = initMessage;
   }
   function update(string memory newMessage) public {
